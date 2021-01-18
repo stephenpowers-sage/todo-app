59f81bdcb14c9fba57cf5f81f6f18779d65f2fcf
Stephen Powers <stephen.powers@sage.com>
2021-01-18 16:30:55 +0000

    Adding user login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
-end
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+  
+    # define current_user and logged_in? as helper methods
+    helper_method :current_user, :logged_in?
+  
+    def current_user
+      # Fing the user that matches the user_id stored in session.
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  
+    def logged_in?
+      # check if the current_user has a value. nil? returns a boolean
+      !current_user.nil?
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # home page
   root to: "site#index"
+  # about page
   get 'about', to: 'site#about'
+
+  # users
   resources :users, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'logout', to: 'sessions#logout'
 end
